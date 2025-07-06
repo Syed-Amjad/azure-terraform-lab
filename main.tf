e245d13150c778d48856ff72987477193595658b
Syed Amjad Ali <143636893+Syed-Amjad@users.noreply.github.com>
2025-07-06 10:43:40 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# Configure Azure Provider
 terraform {
   required_providers {
     azurerm = {
@@ -16,18 +15,22 @@ provider "azurerm" {
   features {}
 }
 
-# Generate random ID for storage account
+# Generate unique suffixes
 resource "random_id" "storage_suffix" {
   byte_length = 4
 }
 
-# 1. Create Resource Group
+resource "random_id" "rg_suffix" {
+  byte_length = 2
+}
+
+# Create Resource Group with unique name
 resource "azurerm_resource_group" "lab4_rg" {
-  name     = "Lab4-RG-Terraform"
+  name     = "lab4-rg-${random_id.rg_suffix.hex}"
   location = "westeurope"
 }
 
-# 2. Create Storage Account (updated with current properties)
+# Storage Account with unique name
 resource "azurerm_storage_account" "lab4_storage" {
   name                     = "lab4storage${random_id.storage_suffix.hex}"
   resource_group_name      = azurerm_resource_group.lab4_rg.name
@@ -40,87 +43,32 @@ resource "azurerm_storage_account" "lab4_storage" {
   https_traffic_only_enabled       = true
   min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
-
-  identity {
-    type = "SystemAssigned"
-  }
 }
 
-# 3. Blob Storage Container
+# Blob Storage Container
 resource "azurerm_storage_container" "lab4_container" {
   name                  = "lab4container"
   storage_account_name  = azurerm_storage_account.lab4_storage.name
   container_access_type = "private"
 }
 
-# Upload a sample file using storage account key
-resource "null_resource" "upload_blob" {
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Hello from Terraform-managed Blob Storage" > blobfile.txt
-      az storage blob upload \
-        --account-name ${azurerm_storage_account.lab4_storage.name} \
-        --container-name ${azurerm_storage_container.lab4_container.name} \
-        --name blobfile.txt \
-        --file blobfile.txt \
-        --auth-mode key \
-        --account-key $(az storage account keys list \
-          --account-name ${azurerm_storage_account.lab4_storage.name} \
-          --resource-group ${azurerm_resource_group.lab4_rg.name} \
-          --query "[0].value" -o tsv)
-    EOT
-  }
-}
-
-# 4. File Share
+# File Share
 resource "azurerm_storage_share" "lab4_fileshare" {
   name                 = "lab4fileshare"
   storage_account_name = azurerm_storage_account.lab4_storage.name
   quota                = 5
 }
 
-# 5. Table Storage
+# Table Storage
 resource "azurerm_storage_table" "lab4_table" {
   name                 = "lab4table"
   storage_account_name = azurerm_storage_account.lab4_storage.name
 }
 
-# Insert sample data using storage account key
-resource "null_resource" "insert_table_data" {
-  provisioner "local-exec" {
-    command = <<EOT
-      az storage entity insert \
-        --table-name ${azurerm_storage_table.lab4_table.name} \
-        --entity PartitionKey=demo RowKey=001 Name=Amjad Score=100 \
-        --account-name ${azurerm_storage_account.lab4_storage.name} \
-        --account-key $(az storage account keys list \
-          --account-name ${azurerm_storage_account.lab4_storage.name} \
-          --resource-group ${azurerm_resource_group.lab4_rg.name} \
-          --query "[0].value" -o tsv)
-    EOT
-  }
-}
-
-# 6. Queue Storage
+# Queue Storage
 resource "azurerm_storage_queue" "lab4_queue" {
   name                 = "lab4queue"
   storage_account_name = azurerm_storage_account.lab4_storage.name
-}
-
-# Send a test message using storage account key
-resource "null_resource" "queue_message" {
-  provisioner "local-exec" {
-    command = <<EOT
-      az storage message put \
-        --queue-name ${azurerm_storage_queue.lab4_queue.name} \
-        --content "Message from Terraform" \
-        --account-name ${azurerm_storage_account.lab4_storage.name} \
-        --account-key $(az storage account keys list \
-          --account-name ${azurerm_storage_account.lab4_storage.name} \
-          --resource-group ${azurerm_resource_group.lab4_rg.name} \
-          --query "[0].value" -o tsv)
-    EOT
-  }
 }
 
 # Outputs
@@ -128,6 +76,10 @@ output "storage_account_name" {
   value = azurerm_storage_account.lab4_storage.name
 }
 
+output "resource_group_name" {
+  value = azurerm_resource_group.lab4_rg.name
+}
+
 output "blob_url" {
-  value = "${azurerm_storage_account.lab4_storage.primary_blob_endpoint}${azurerm_storage_container.lab4_container.name}/blobfile.txt"
+  value = "${azurerm_storage_account.lab4_storage.primary_blob_endpoint}${azurerm_storage_container.lab4_container.name}"
 }
